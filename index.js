6708e2f4a1e3d7ccbeaea31a14645447f0403e99
Robbie <akotoro22@gmail.com>
2018-03-29 17:16:40 +0000

    made changes to the page view

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
-    res.send("Welcome to my homepage");
+    res.sendFile('landing.html');
 });
 app.listen(port, function() {
     console.log("my webpage is served on port", port);
