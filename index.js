73a1d362c50766d480c83acfeb8d5c1cf26c033b
Robbie <akotoro22@gmail.com>
2018-03-29 16:34:32 +0000

    changes were made to the listen port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 3000 || process.env.PORT;
+var port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
     res.send("Welcome to my homepage");
 });
