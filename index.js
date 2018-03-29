4a0533b7d4fba40c13def7d77fb14f442f2f5562
Robbie <akotoro22@gmail.com>
2018-03-29 16:21:34 +0000

    made changes to the port details

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-//app.set('port', 8080);
+var port = 3000 || process.env.PORT;
 app.get('/', function(req, res) {
     res.send("Welcome to my homepage");
 });
 app.listen(3000, function() {
-    console.log("my webpage is served on port", 3000);
+    console.log("my webpage is served on port", port);
 });
