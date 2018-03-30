d270df046769d4cdd4ea2c709a7fe196d13ac550
Robbie <akotoro22@gmail.com>
2018-03-30 20:34:03 +0000

    Changed the path name

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var path = require('path');
+
+//var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
-    res.sendFile('landing.html');
+    res.sendFile(path.join(__dirname + '/landing.html'));
 });
+
 app.listen(port, function() {
     console.log("my webpage is served on port", port);
 });
