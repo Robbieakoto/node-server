03c6330808c1ae07fe7ddaf3e2a25f32d10353f8
Robbie <akotoro22@gmail.com>
2018-03-30 20:45:46 +0000

    changes made to the path

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/landing.html'));
 });
-/*app.listen(port, function() {
+app.listen(port, function() {
     console.log("my webpage is served on port", port);
-});*/
-app.listen(3000);
+});
